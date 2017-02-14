734cab48eb6770ef6a7b896a244948557eb59928
Bill <jtbuestc@gmail.com>
2017-02-14 16:58:52 +0800

    1. completed the triangle and it's test

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,6 +1,33 @@
 public class Triangle {
 
+    public void easist() {
 
+        System.out.println("*");
+    }
 
+    public void horizontal(int n) {
+
+        while (n-- > 0) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    public void vertical(int n) {
+        while (n-- > 0) {
+            System.out.println("*");
+        }
+    }
+
+    public void righttriangle(int n) {
+
+        for (int i = 1; i <= n; i++) {
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
 
 }
--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -1,3 +1,59 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
 public class TriangleTest {
+
+    Triangle triangle;
+    PrintStream console = null;          // 声明（为null）：输出流 (字符设备) console
+    ByteArrayOutputStream bytes = null;  // 声明（为null）：bytes 用于缓存console 重定向过来的字符流
+
+    @Before
+    public void initial(){
+        triangle = new Triangle();
+        bytes = new ByteArrayOutputStream();    // 分配空间
+        console = System.out;                   // 获取System.out 输出流的句柄
+        System.setOut(new PrintStream(bytes));
+    }
+
+    @After
+    public void restore(){
+        System.setOut(console);
+    }
+
+
+    @Test
+    public void TestEasist(){
+        triangle.easist();
+        assertEquals("*\n",bytes.toString());
+    }
+
+    @Test
+    public void Testhorizontal(){
+        triangle.horizontal(8);
+        assertEquals("********\n",bytes.toString());
+    }
+
+
+
+    @Test
+    public void Testvertical(){
+        triangle.vertical(8);
+        assertEquals("*\n*\n*\n*\n*\n*\n*\n*\n",bytes.toString());
+    }
+
+
+
+    @Test
+    public void TestRightTran(){
+        triangle.righttriangle(3);
+        assertEquals("*\n**\n***\n",bytes.toString());
+    }
+
 
 }
